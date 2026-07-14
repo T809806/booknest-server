13a28f5b2e6d420649aa8cf07ede05cbe9adb509
T806809 <tahiyaakter94@gmail.com>
2026-07-15 02:12:39 +0600

    some bug fix

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,25 @@
+
 import express from "express";
 import cors from "cors";
-
 import authRoutes from "./routes/auth.routes";
 import bookRoutes from "./routes/book.routes";
-
 const app = express();
-
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://book-nest-client-mxlxgsl5g-t809806s-projects.vercel.app",
+    ],
     credentials: true,
   })
 );
-
 app.use(express.json());
-
 // Home Route
 app.get("/", (req, res) => {
-  res.send("BookNest Server is Running");
+res.send("BookNest Server is Running");
 });
-
 // API Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/books", bookRoutes);
-
 export default app;
