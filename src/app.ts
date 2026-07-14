1d3d3074824905bc435b77ad2a259a274278a887
T806809 <tahiyaakter94@gmail.com>
2026-07-15 04:00:49 +0600

    some bug fix

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://book-nest-client-nu.vercel.app/",
+      "https://book-nest-client-nu.vercel.app",
     ],
     credentials: true,
   })
